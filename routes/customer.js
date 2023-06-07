29110c9646c4a812debad76adba5755e5d1321f6
swapnilbhadoriya10@gmail.com <swapnilbhadoriya10@gmail.com>
2023-06-07 16:33:21 +0530

    Check

--- routes/customer.js
+++ routes/customer.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const {submitCustomerDetails} = require('../controllers/customer')
 
 router.post('/',submitCustomerDetails);
+router.get('/',function(req,res){return res.json({msg:"Working"})})
 
 module.exports = router;
